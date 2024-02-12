7111e8a65c5956ef5deb0b8eaf9b8b2367880347
arthdarji352 <arthdarji20@gmail.com>
2024-02-12 13:39:49 +0530

    add navbar & Searchbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import SearchBox from "../searchBox/SearchBox";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // navList Data
+  const navList = (
+    <ul className="flex space-x-3 text-white font-medium text-md px-5 ">
+      <li>
+        <Link to={"/"}>Home</Link>
+      </li>
+      <li>
+        <Link to={"/allproduct"}>All Product</Link>
+      </li>
+      <li>
+        <Link to={"/signup"}>Signup</Link>
+      </li>
+      <li>
+        <Link to={"/"}>Kamal</Link>
+      </li>
+      {/* Admin */}
+      {/* <li>
+        <Link to={"/"}>Admin</Link>
+      </li> */}
+      {/* logout */}
+      {/* <li>
+      Logout
+        </li> */}
+      {/* Cart */}
+      <li>
+        <Link to={"/cart"}></Link>
+      </li>
+    </ul>
+  );
+  return (
+    <div>
+      <nav className="bg-pink-600 sticky top-0">
+        {/* main  */}
+        <div className="lg:flex lg:justify-between items-center py-3 lg:px-3 ">
+          {/* left  */}
+          <div className="left py-3 lg:py-0">
+            <Link to={"/"}>
+              <h2 className=" font-bold text-white text-2xl text-center">
+                Earth-Store
+              </h2>
+            </Link>
+          </div>
+          {/* right  */}
+          <div className="right flex justify-center mb-4 lg:mb-0">
+            {navList}
+          </div>
+          {/* Search Bar  */}
+          <SearchBox />
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
