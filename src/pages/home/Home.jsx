6a90801960ebd7d96aa612dca15bfbae9a07a195
arthdarji352 <arthdarji20@gmail.com>
2024-02-13 17:29:07 +0530

    add hero section adn category

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
+import HeroSection from "../../components/heroSection/HeroSection";
+import Category from "../../components/category/Category";
 
 const Home = () => {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <HeroSection />
+      <Category />
+    </Layout>
+  );
 };
 
 export default Home;
