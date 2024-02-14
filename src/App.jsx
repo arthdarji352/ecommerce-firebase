9757370cc5c47d10b1ee0458749dd6bb6a2d7790
arthdarji352 <arthdarji20@gmail.com>
2024-02-14 12:29:11 +0530

    create cart,all products,productInfo pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/Home";
 import NoPage from "./pages/noPage/NoPage";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
+import CartPage from "./pages/cart/CartPage";
+import AllProduct from "./pages/allProduct/AllProduct";
 
 const App = () => {
   return (
     <>
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
+          <Route path="/productinfo" element={<ProductInfo />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/allproduct" element={<AllProduct />} />
         </Routes>
       </Router>
     </>
